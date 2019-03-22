9fee0d625437cb30a5587582eb3a595717d23cfe
Зальцман Никита <NikitaZaltsman@gmail.com>
2019-03-23 01:36:34 +0700

    + add date convertor

--- app/src/main/java/ru/coderedwolf/bookplayer/domain/BookPlayerDataBase.kt
+++ app/src/main/java/ru/coderedwolf/bookplayer/domain/BookPlayerDataBase.kt
@@ -2,11 +2,14 @@ package ru.coderedwolf.bookplayer.domain
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import ru.coderedwolf.bookplayer.domain.converters.DateConverter
 import ru.coderedwolf.bookplayer.domain.model.Book
 import ru.coderedwolf.bookplayer.domain.model.BookFile
 import ru.coderedwolf.bookplayer.domain.repositories.BookRepository
 
 @Database(entities = [Book::class, BookFile::class], version = 1)
+@TypeConverters(value = [DateConverter::class])
 abstract class BookPlayerDataBase : RoomDatabase() {
 
     abstract fun bookRepository(): BookRepository
