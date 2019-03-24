1751c8d81308171751cc79764434f38506202a2e
Зальцман Никита <NikitaZaltsman@gmail.com>
2019-03-24 22:18:58 +0700

    * refactoring audio file manager

--- app/src/main/java/ru/coderedwolf/bookplayer/managers/AudioFileManager.kt
+++ app/src/main/java/ru/coderedwolf/bookplayer/managers/AudioFileManager.kt
@@ -5,24 +5,25 @@ import android.content.ContentUris
 import android.content.Context
 import android.net.Uri
 import android.provider.MediaStore
-import ru.coderedwolf.bookplayer.domain.model.AudioFile
+import ru.coderedwolf.bookplayer.domain.model.AudioData
 import ru.coderedwolf.bookplayer.extensions.toList
 
 
 interface AudioFileManager {
 
-    suspend fun findAll(): List<AudioFile>
+    suspend fun findAll(): List<AudioData>
 }
 
 class AudioFileManagerImpl(private val context: Context) : AudioFileManager {
 
     companion object {
 
-        private const val CONTENT_PATH_ALBUM_ART = "content://media/external/audio/albumart"
+        private val ALBUM_ART_URI =
+                Uri.parse("content://${MediaStore.AUTHORITY}/external/audio/albumart")
     }
 
     @SuppressLint("Recycle")
-    override suspend fun findAll(): List<AudioFile> {
+    override suspend fun findAll(): List<AudioData> {
 
         val selection = MediaStore.Audio.Media.IS_MUSIC + " != 0"
 
@@ -44,19 +45,17 @@ class AudioFileManagerImpl(private val context: Context) : AudioFileManager {
                 null
         )
 
-        val albumArtUri = Uri.parse(CONTENT_PATH_ALBUM_ART)
-
         return queryCursor.toList { cursor ->
             val albumId = cursor.getLong(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM_ID))
-            val imageFileUri = ContentUris.withAppendedId(albumArtUri, albumId)
+            val imageFileUri = ContentUris.withAppendedId(ALBUM_ART_URI, albumId)
 
-            return@toList AudioFile(
+            return@toList AudioData(
                     id = cursor.getLong(cursor.getColumnIndex(MediaStore.Audio.Media._ID)),
                     name = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE)),
                     author = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST)),
                     duration = cursor.getLong(cursor.getColumnIndex(MediaStore.Audio.Media.DURATION)),
                     filePath = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA)),
-                    imageUri = imageFileUri
+                    imageUri = imageFileUri.toString()
             )
         }
     }
